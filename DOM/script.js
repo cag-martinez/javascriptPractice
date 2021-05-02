989e8f47a3b5eeb73fd090d5fb861a9eefc0040e
Christian Gomez Martinez <gomez9289@gmail.com>
2021-05-02 15:25:09 -0400

    decoration

--- DOM/script.js
+++ DOM/script.js
@@ -1,13 +1,15 @@
 let article = document.getElementById('articles');
 let main = document.getElementById('main');
 
-article.children[0].style.fontSize="50px";
-article.children[0].lastElementChild.style.Color="blue";
-article.previousElementSibling.style.backgroundColor="black";
+article.children[0].style.fontSize="100px";
+article.children[0].lastElementChild.style.color="red";
+article.previousElementSibling.style.backgroundColor="gray";
 
 main.childNodes[1].style.textDecoration="underline";
-main.lastElementChild.style.fontSize="50px"
-main.firstElementChild.style.color="orange";
-main.lastElementChild.parentElement.style.fontSize="40px"
+main.lastElementChild.style.fontSize="10px"
+main.lastElementChild.style.textDecoration="underline"
+main.firstElementChild.style.color="black";
+main.lastElementChild.parentElement.style.fontSize="30px"
 
-article.children[1].lastElementChild.style.border="red"
+article.children[0].lastElementChild.style.border="dotted"
+article.children[0].style.textDecoration="dotted"
