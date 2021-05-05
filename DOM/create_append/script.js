b12185f9cfe356a110a4ad316939f18d6a45680d
Christian Gomez Martinez <gomez9289@gmail.com>
2021-05-05 09:51:01 -0400

    SetAttribute styles

--- DOM/create_append/script.js
+++ DOM/create_append/script.js
@@ -6,6 +6,7 @@ var body = document.body;
 
 var h1tag = document.createElement('h1');
 var h2tag = document.createElement('h2');
+var infoDiv = document.createElement('div');
 var image = document.createElement('img');
 var ptag =  document.createElement('p');
 var list = document.createElement('ol');
@@ -30,18 +31,28 @@ li3.textContent='Watch TV together';
 
 // Append all of the elements
 
-document.body.appendChild(h1tag);
-document.body.appendChild(h2tag);
-document.body.appendChild(ptag);
-document.body.appendChild(list);
-document.body.appendChild(li1);
-document.body.appendChild(li2);
-document.body.appendChild(li3);
+body.appendChild(h1tag);
+body.appendChild(h2tag);
+body.appendChild(infoDiv)
+infoDiv.appendChild(image)
+infoDiv.appendChild(ptag);
+//document.body.appendChild(image);
+
+infoDiv.appendChild(list);
+list.appendChild(li1);
+list.appendChild(li2);
+list.appendChild(li3);
 
 // Style all of the elements
 
 h1tag.setAttribute('style', 'text-align: center')
 h2tag.setAttribute('style', 'text-align: center')
+infoDiv.setAttribute('style', 'margin: auto; width: 50%; text-align: center')
+ptag.setAttribute('style', 'text-align: center; text-decoration: underline; color: orange')
+image.setAttribute('src', 'http://placekitten.com/200/300')
+image.setAttribute("height", 200);
+image.setAttribute("width", 200);
+list.setAttribute('style', 'background-color: cornsilk; text-decoration: underline')
 
 
 
