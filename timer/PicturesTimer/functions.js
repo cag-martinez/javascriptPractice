285f81cffcdacd3b9cba339ca1d4637ba1a4b085
Christian Gomez Martinez <gomez9289@gmail.com>
2021-05-05 23:00:07 -0400

    Timer displaying images

--- timer/PicturesTimer/functions.js
+++ timer/PicturesTimer/functions.js
@@ -1,9 +1,28 @@
-var main = document.querySelector('main');
-var pictueres = document.querySelector('pictures');
+var main = document.querySelector(".main");
+var pictures = document.querySelector(".pictures");
 
-var timeRemaining = 5
+var timeRemaining = 5;
 
-function timer(){
-    secondsLeft--;
-    
+function setTime() {
+  var timerInterval = setInterval(function () {
+    timeRemaining--;
+    main.textContent =
+      timeRemaining + "seconds remaining before the next picture!!";
+    if (timeRemaining === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 500);
 }
+
+function sendMessage() {
+  main = "";
+
+  var imgElement = document.createElement("img");
+  imgElement.setAttribute("src", "images/car1.jpg");
+  //fix the style
+  //imgElement.setAttribute("style", 'align-items: center')
+  pictures.appendChild(imgElement);
+}
+
+setTime();
