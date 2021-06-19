04af0655c37198fe7c98e0eeacf77231d1364c00
Christian Gomez Martinez <gomez9289@gmail.com>
2021-06-19 14:47:43 -0500

    warmup

---
+++ refresher/refresher.js
@@ -0,0 +1,36 @@
+// var a = 5;
+// var b = 6;
+// var c = 0;
+
+// var sum = 10+10;
+// sum++;
+// console.log(sum)
+
+
+// c = a + b;
+// console.log(a)
+ 
+var concat = "im first ";
+var tenate = "second";
+var together = concat+tenate;
+
+var newLine = "im the first part " + "im the second part";
+
+concat += "im here to stay forever"
+console.log(concat)
+
+var myName = "christian";
+
+var sentence = "Hi my name is " + myName + concat + " and " + newLine;
+
+// var length = 0
+// var array = [1,2,3,4,5,6]
+// length = array.length;
+// console.log(length)
+
+var firstLetterOfName = myName[0]
+var secondToLast = myName[myName.length - 2];
+var fourthFromLast = myName[myName.length - 4];
+var addI = myName[myName.push(i)[0]]
+
+
