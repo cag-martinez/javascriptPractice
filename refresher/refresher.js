8992668fad2e0c44d63a96e5db7a0e00d01fa868
Christian Gomez Martinez <gomez9289@gmail.com>
2021-06-19 17:00:18 -0500

    arrays

--- refresher/refresher.js
+++ refresher/refresher.js
@@ -1,36 +1,22 @@
-// var a = 5;
-// var b = 6;
-// var c = 0;
 
-// var sum = 10+10;
-// sum++;
-// console.log(sum)
+//Madlibs
+// function libs(noun, adj, verb, adverb) {
+//     var results = " ";
+//     results += "the " + adj + noun + verb + adverb + " to the store";
+//     return results;
+// }
+// console.log(libs("dog", "big", "ran", "quickly"))
 
 
-// c = a + b;
-// console.log(a)
- 
-var concat = "im first ";
-var tenate = "second";
-var together = concat+tenate;
+// function names(one){
+//     var allNames = " ";
+//     allNames += "all names are " + one + "!!";
+//     return allNames;
+// }
+// console.log(names("Chris"))
 
-var newLine = "im the first part " + "im the second part";
+// var myArray = [[1,2,3,4,5],[6,7,8,9]];
+// var remove = myArray.shift();
 
-concat += "im here to stay forever"
-console.log(concat)
-
-var myName = "christian";
-
-var sentence = "Hi my name is " + myName + concat + " and " + newLine;
-
-// var length = 0
-// var array = [1,2,3,4,5,6]
-// length = array.length;
-// console.log(length)
-
-var firstLetterOfName = myName[0]
-var secondToLast = myName[myName.length - 2];
-var fourthFromLast = myName[myName.length - 4];
-var addI = myName[myName.push(i)[0]]
-
+// console.log(remove)
 
