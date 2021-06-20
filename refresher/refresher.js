ee290684d5cd01b45af2623d20e80bbeeca209ee
Christian Gomez Martinez <gomez9289@gmail.com>
2021-06-19 20:21:49 -0500

    Switch card game

--- refresher/refresher.js
+++ refresher/refresher.js
@@ -101,84 +101,138 @@
 // console.log("Before: " + (testArr))
 // console.log(two(testArr,7))
 // console.log("after: "+ (testArr))
+// //================================================================
+// function doesEqual(a, b) {
+//   if (a === b) {
+//     return "Yes, they are equal";
+//   } else {
+//     return "No, they are not equal";
+//   }
+// }
+// console.log(doesEqual(5, 5));
 
-function doesEqual(a, b) {
-  if (a === b) {
-    return "Yes, they are equal";
-  } else {
-    return "No, they are not equal";
-  }
-}
-console.log(doesEqual(5, 5));
+// function another(c, d) {
+//   if (c === d) {
+//     return "Yup, we are equal";
+//   } else {
+//     return "Nope, we are not the same";
+//   }
+// }
+// console.log(another(2, "a"));
 
-function another(c, d) {
-  if (c === d) {
-    return "Yup, we are equal";
-  } else {
-    return "Nope, we are not the same";
-  }
-}
-console.log(another(2, "a"));
+// function comparison(value) {
+//   if (value <= 10 && value >= 5) {
+//     return "True";
+//   } else {
+//     return "false";
+//   }
+// }
+// console.log(comparison(6));
+// console.log(comparison(3));
 
-function comparison(value) {
-  if (value <= 10 && value >= 5) {
-    return "True";
-  } else {
-    return "false";
-  }
-}
-console.log(comparison(6));
-console.log(comparison(3));
+// function compareTwo(val) {
+//   if (val == 3 || val == 5) {
+//     return "yes im  equal";
+//   } else {
+//     return "nope, try again";
+//   }
+// }
+// console.log(compareTwo(3));
+// console.log(compareTwo(4));
 
-function compareTwo(val) {
-  if (val == 3 || val == 5) {
-    return "yes im  equal";
-  } else {
-    return "nope, try again";
-  }
-}
-console.log(compareTwo(3));
-console.log(compareTwo(4));
+// function newfunction(number) {
+//   if (number < 5) {
+//     return "Less than 5";
+//   } else if (number > 10) {
+//     return " greater than 10";
+//   } else {
+//     return "the number is between 5 and 10";
+//   }
+// }
+// console.log(newfunction(7));
+// console.log(newfunction(25));
+// console.log(newfunction(1));
 
-function newfunction(number) {
-  if (number < 5) {
-    return "Less than 5";
-  } else if (number > 10) {
-    return " greater than 10";
-  } else {
-    return "the number is between 5 and 10";
-  }
-}
-console.log(newfunction(7));
-console.log(newfunction(25));
-console.log(newfunction(1));
+// var names = [
+//   "Hole-in-one",
+//   "eagle",
+//   "Birdie",
+//   "Par",
+//   "Bogie",
+//   "Double-bougie",
+//   "Go home",
+// ];
+// function golfScore(par, strokes) {
+//   if (strokes == 1) {
+//     return names[0];
+//   } else if (strokes <= par - 2) {
+//     return names[1];
+//   } else if (strokes <= par - 1) {
+//     return names[2];
+//   } else if (strokes == par) {
+//     return names[3];
+//   } else if (strokes == par + 1) {
+//     return names[4];
+//   } else if (strokes == par + 2) {
+//     return names[5];
+//   } else if (strokes >= par + 3) {
+//     return names[6];
+//   } else {
+//     return "Change me";
+//   }
+// }
+// console.log(golfScore(5, 4));
 
-var names = [
-  "Hole-in-one",
-  "eagle",
-  "Birdie",
-  "Par",
-  "Bogie",
-  "Double-bougie",
-  "Go home",
-];
-function golfScore(par, strokes) {
-  if (strokes == 1) {
-    return names[0];
-  } else if (strokes <= par - 2) {
-    return names[1];
-  } else if (strokes <= par - 1) {
-    return names[2];
-  } else if (strokes == par) {
-    return names[3];
-  } else if (strokes == par + 1) {
-    return names[4];
-  } else if (strokes == par + 2) {
-    return names[5];
-  } else if (strokes >= par + 3) {
-    return names[6];
-  } else {
-    return "Change me";
-  }
+
+function checkSwitch(value){
+    var answer = ""
+    switch(value){
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "tango";
+        default:
+            answer = "Nothing here"
+    }
+    return answer
 }
-console.log(golfScore(5, 4));
+console.log(checkSwitch(6))
+//=======================================
+var count = 0
+
+function cc(card){
+    switch(card){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:    
+        case 7:
+        case 8:
+        case 9:
+            count++    
+            break;
+        case 10:
+        case "K":
+        case "Q":
+        case "J":
+        case "A":
+            count--
+            break;       
+    }
+    
+
+    var bet = "hold";
+    if (count > 0){
+        bet = "bet!"
+    }
+    return count + '' + bet;
+}
+cc(22); cc(6); cc(18); cc("K"); cc("A")
+console.log(cc(4))
+
